0d34882cb6b8970c8d375a7fb997312b98787b53
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-30 17:46:01 +0000

    starting fresh

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,10 @@ app.use(express.json());
 
 const port = 4000;
 
-const people = [
-    { name: "Jena", isPresent: true },
-    { name: "Kristina", isPresent: true },
-    { name: "Kukovic", isPresent: false }
-];
+app.get("/", (req,res)=> {
 
-app.get("/api/content", (req, res) => {
-    res.send(people);
-});
+} )
 
-app.post("/api/content", (req, res) => {
-    const newPerson = req.body;
-    people.push(newPerson);
-    res.status(201).send(newPerson);
-});
-
-app.put("/api/content/:name", (req, res) => {
-    const name = req.params.name;
-    const { isPresent } = req.body;
-
-    const person = people.find((p) => p.name === name);
-
-    if (person) {
-        person.isPresent = isPresent;
-        res.status(200).json(person);
-    } else {
-        res.status(404).json({ message: "Person not found" });
-    }
-});
-
-app.listen(port, () => {
-    console.log("listening on port 4000");
-});
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
