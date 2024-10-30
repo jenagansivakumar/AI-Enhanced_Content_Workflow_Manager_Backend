86ca1d7e04f3f425a573810e9e5e777e4898c419
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-30 17:10:44 +0000

    changed package.json to include es6 modules

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,28 @@ app.use(express.json())
 
 const port = 4000
 
-const people = [{
-    "Jena": true,
-    "Kristina": true,
-    "Kukovic": false
-}]
+const people = [
+    { name: "Jena", isPresent: true },
+    { name: "Kristina", isPresent: true },
+    { name: "Kukovic", isPresent: false }
+  ];
 
 app.get("/api/content", (req, res)=>{
     res.send(people)
 } )
 
+app.post("/api/content", (req, res)=>{
+    const newPerson = req.body
+    console.log(req.body)
+    people.push(newPerson)
+    console.log("Current People Array:", people); 
+    res.status(201).send(newPerson)
+})
+
+app.put("/api/content/:id", (req,res)=>{
+    const id = req.params.id
+
+})
 app.listen(port, ()=>{
     console.log("listening on port 4000")
 })
