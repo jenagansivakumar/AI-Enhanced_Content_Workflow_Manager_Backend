4d4901017553465782aef28ccbe28c94fd88dc6f
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-30 17:50:13 +0000

    created mock data

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,21 @@ app.use(express.json());
 
 const port = 4000;
 
-app.get("/", (req,res)=> {
+interface ContentItem {
+    id: number;
+    title: string;
+    body: string;
+    status: "draft" | "review" | "published";
+    tags?: string[];
+}
 
+const contentList: ContentItem [] = [
+    {id: 1, title: "Test", body: "Test body", status: "draft", tags: ["test tag", "test tag2"]},
+    {id: 2, title: "Test 2", body: "Test body 2 ", status: "draft", tags: ["test tag 2", "test tag 3"]}
+]
+
+app.get("/", (req,res)=> {
+    res.send()
 } )
 
 app.listen(port, ()=>{
