a301efc5a8fa576983989c54594e73173afc9d18
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-11-04 23:47:00 +0000

    cron job is working

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,34 @@ import cors from 'cors';
 import contentRoutes from './routes/contentRoutes';
 import { connectDB } from './config/database';
 import errorHandler from './middlewares/errorHandler';
+import Content from './models/contentModels';
+
+
 
 dotenv.config();
 const app = express();
+const cron = require('node-cron');
+const SEVEN_DAYS_IN_MS = 7 * 24 * 60 * 60 * 1000;
+
+cron.schedule('0 0 * * *', async () => {
+    console.log('Running scheduled task...');
+
+    const now = new Date().getTime();
+
+    try {
+        const contentItems = await Content.find({ status: 'draft' });
+
+        contentItems.forEach(async (item) => {
+            if (item.createdAt && now - new Date(item.createdAt).getTime() > SEVEN_DAYS_IN_MS) {
+                item.status = 'review';
+                await item.save();
+                console.log(`Content ID ${item._id} status updated to review`);
+            }
+        });
+    } catch (error) {
+        console.error('Error in scheduled task:', error);
+    }
+});
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
